5ed49286153869210a7009105f2b6b3258ace52a
chikitaS <chikita.salsabiil.tif20@polban.ac.id>
2022-04-04 10:36:05 +0700

    adding SimpleAsyncTask project

---
+++ SimpleAsyncTask/app/src/main/java/com/example/simpleasynctask/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.simpleasynctask;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TEXT_STATE = "currentText";
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTextView = findViewById(R.id.textView1);
+        // Restore TextView if there is a savedInstanceState
+        if(savedInstanceState!=null){
+            mTextView.setText(savedInstanceState.getString(TEXT_STATE));
+        }
+    }
+
+    public void startTask(View view) {
+        // Put a message in the text view
+        mTextView.setText(R.string.napping);
+        // Start the AsyncTask.
+        new SimpleAsyncTask(mTextView).execute();
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // Save the state of the TextView
+        outState.putString(TEXT_STATE,
+                mTextView.getText().toString());
+    }
+}
